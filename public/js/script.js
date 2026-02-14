0ef985056873d15685bee34627f739e57ff9ac2f
ttannishh-afk <tanishgupta69@gmail.com>
2026-02-14 09:19:56 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -62,7 +62,7 @@ no_button.addEventListener('click', () => {
 yes_button.addEventListener('click', () => {
     // change banner gif path
     let banner = document.getElementById('banner');
-    banner.src = "public/images/yes.gif";
+    banner.src = "public/images/yess.gif";
     refreshBanner();
     // hide buttons div
     let buttons = document.getElementsByClassName('buttons')[0];
