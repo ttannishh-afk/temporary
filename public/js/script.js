0633c4c443ab3e879338d7109f782a8a53af4882
ttannishh-afk <tanishgupta69@gmail.com>
2026-02-14 09:28:39 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -5,15 +5,12 @@ const answers_no = {
         "Are you really sure??",
         "Are you really realy sure???",
         "Think again?",
-        "Don't believe in second chances?",
         "Why are you being so cold?",
         "Maybe we can talk about it?",
         "I am not going to ask again!",
         "Ok now this is hurting my feelings!",
         "You are now just being mean!",
         "Why are you doing this to me?",
-        "Please give me a chance!",
-        "I am begging you to stop!",
         "Ok, Let's just start over.."
     ]
 };
